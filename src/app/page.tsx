e7b974499ed50c46950ededb11007fb64945141b
Ian Robertson <ijr011235@gmail.com>
2025-07-24 13:53:05 -0600

    Full page smooth scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,28 @@ export default function Home() {
     <div className="min-h-screen flex">
       <div className="flex-1"> 
         <main className="min-h-screen flex flex-col items-center">
-          <div className="flex-1 w-full flex flex-col gap-20 items-center">
-
-          </div>
           <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-            <main className="flex-1 flex flex-col gap-6 px-4">
+            <main className="flex-1 flex flex-col">
+              <section id='me' className="min-h-screen flex items-center justify-center px-4">
                 <AnimatedSection>
-                    <h1 className="text-4xl font-bold">Hi, I&apos;m Ian 👋</h1>
-                    <p className="mt-2 text-muted-foreground">Full-stack developer | React | TypeScript</p>
+                  <h1 className="text-4xl font-bold">Hi, I&apos;m Ian 👋</h1>
+                  <p className="mt-2 text-muted-foreground">Full-stack developer | React | TypeScript</p>
                 </AnimatedSection>
-                    
-                <Projects/>
-                <Experience/>
-                <Contact/>
+              </section>
+
+              <section id="experience" className="min-h-screen flex items-center px-4">
+                <Experience />
+              </section>
+
+              <section id="projects" className="min-h-screen flex items-center px-4">
+                <Projects />
+              </section>
+
+              <section id="contact" className="min-h-screen flex items-center justify-center px-4">
+                <Contact />
+              </section>
             </main>
           </div>
-
           <footer className="w-full flex items-center justify-center mx-auto text-center text-xs gap-8 py-3">
             <p>
               Created by{" "}
