6816bb10a5fb0483646a6aadc8b11b18a5c5f3ed
Ian Robertson <ijr011235@gmail.com>
2025-07-27 19:48:16 -0600

    Updatedd copy email

--- src/app/Contact.tsx
+++ src/app/Contact.tsx
@@ -2,33 +2,34 @@
 
 import AnimatedSection from "@/components/AnimatedSection";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Contact() {
+    const [copied, setCopied] = useState(false);
 
-    const sendEmail = async () => {
-        const response = await fetch('/api/contact', {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-        })
-
-        if (!response.ok) {
-        throw new Error('Network response was not ok')
+    const handleCopy = async () => {
+        try {
+            await navigator.clipboard.writeText('ianjr@byu.edu');
+            setCopied(true);
+            setTimeout(() => setCopied(false), 1500);
+        } catch (err) {
+            console.error('Failed to copy: ', err);
         }
-
-        const result = await response.json()
-        console.log(result);
-    }
+    };
 
     return (
         <AnimatedSection delay={0.6}>
             <h2 className="text-2xl font-semibold mb-2">Get In Touch</h2>
             <p className="mb-4 text-muted-foreground">Interested in working together? Let&apos;s chat.</p>
             <div className="flex flex-col sm:flex-row gap-4">
-                <Button asChild onClick={sendEmail}>
-                    <span>Contact Me</span>
+                <Button asChild onClick={handleCopy} className="cursor-pointer">
+                    {copied ? <p>Copied!</p> : <p>ianjr@byu.edu</p>}
+                </Button>
+                <Button asChild>
+                    <a href="https://linkedin.com/in/ianjosephrobertson" target="_blank">LinkedIn</a>
                 </Button>
                 <Button variant="outline" asChild>
-                    <a href="/Robertson-Ian-Resume-7-25.pdf" download>Download Resume</a>
+                    <a href="/Public-Robertson-Ian-Resume-7-25.pdf" download>Download Resume</a>
                 </Button>
             </div>
         </AnimatedSection>
