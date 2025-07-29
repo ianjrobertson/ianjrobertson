e701b96553e1e376b9e65aaa9637edf2e7086baf
Ian Robertson <ijr011235@gmail.com>
2025-07-29 11:13:32 -0600

    started building aboutMe

---
+++ src/app/AboutMe.tsx
@@ -0,0 +1,11 @@
+import AnimatedSection from "@/components/AnimatedSection";
+
+export default function AboutMe() {
+
+    return (
+        <AnimatedSection delay={0.6}>
+            <h2 className="text-2xl font-semibold mb-2">About Me</h2>
+            <p></p>
+        </AnimatedSection>
+    )
+}
